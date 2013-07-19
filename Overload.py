9738364af865235002352978d5bfcba9230d4a8b
iHeartPie <eweaver15@comcast.net>
2013-07-19 14:55:21 -0500

    Updated comments

--- Overload.py
+++ Overload.py
@@ -17,21 +17,21 @@ def main():
 
     line_one = ""   #This program searches 2 lines, the strings here will hold them during the for loop
     line_two = ""
-    counter = 0     #this counts to search 2 lines from the text file
+    counter = 0     #this is used in the for loop 
     blast_counter = 1 #this counts the number of blast searches performed
     for lines in my_string_to_use:
         if counter == 0:
             counter = counter + 1
-            #print "pear"
-        elif counter%2 == 1:  #if the counter is odd
+            #print "pear"  #These random print statements were used for troubleshooting
+        elif counter%2 == 1:  #used if the counter is odd, it saves the line of nucleotides for later
             line_one = lines
             counter = counter+1
             #print "apple"
-        elif counter%2 == 0:  #if the counter is even, it will blast search both lines and clear the temporary lines
+        elif counter%2 == 0:  # if the counter is even, it will blast search both lines and clear the temporary lines
             line_two = lines
             result_handle = NCBIWWW.qblast("blastn", "nt", line_one + line_two)
             #print "purple"
-            print result_handle
+            print result_handle  #This tells you in the python terminal if a blast search was performed
             save_file = open("C:/Users/Evan/Desktop/overload_genome.xml", "w")
             save_file.write(result_handle.read())
             save_file.close()
